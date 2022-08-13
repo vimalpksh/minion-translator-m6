37dd4db0217214e21520aeba16f45831fdf90ba5
vimalpksh <itisvimal@gmail.com>
2022-08-14 01:00:34 +0530

    test : changed some variables

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ var serverURL = "https://api.funtranslations.com/translate/minion.json";
 function constructURL(input) {
   return serverURL + "?" + "text=" + input;
 }
-// function clickHandler() {
-//   var inputText = textInput.value;
-
-//   fetch(constructURL(inputText))
-//     .then((response) => response.json())
-//     .then((json) => {
-//       var translatedText = json.contents.translated;
-//       outputBox.innerText = translatedText;
-//     });
-// }
-// btnTranslate.addEventListener("click", clickHandler);
 
 function clickHandler() {
   var inpText = textInput.value;
@@ -33,27 +22,3 @@ function clickHandler() {
     });
 }
 btnTranslate.addEventListener("click", clickHandler);
-
-// function getTranslationURL(input) {
-//   return serverURL + "?" + "text=" + input;
-// }
-
-// // function errorHandler(error) {
-// //   console.log("error occured", error);
-// //   alert("something wrong with server! try again after some time");
-// // }
-
-// function clickHandler() {
-//   var inputText = textInput.value; // taking input
-
-//   // calling server for processing
-//   fetch(getTranslationURL(inputText))
-//     .then((response) => response.json())
-//     .then((json) => {
-//       var translatedText = json.contents.translated;
-//       outputBox.innerText = translatedText; // output
-//     });
-//   // .catch(errorHandler);
-// }
-
-// btnTranslate.addEventListener("click", clickHandler);
